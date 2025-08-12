4125d21f1b6aee2f6ebdd6ef6eee56a415aa333c
vanyastaff <ivan.kondrashkin@gmail.com>
2025-08-12 04:05:00 -0700

    impl types for nebula-value

---
+++ crates/nebula-value/src/lib.rs
@@ -0,0 +1,11 @@
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![allow(missing_docs)]
+#![warn(clippy::all)]
+
+#![cfg_attr(not(feature = "std"), no_std)]
+extern crate alloc;
+
+mod core;
+mod types;
+mod validation;
+pub use core::*;
