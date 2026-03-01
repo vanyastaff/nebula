e976b90030061c2839d9a53e138974867c4b59c0
vanyastaff <ivan.kondrashkin@gmail.com>
2026-03-01 05:37:48 -0600

    refactor(desktop): delete Manager-class infrastructure

--- apps/desktop/src/application/services.ts
+++
@@ -1,8 +0,0 @@
-import { AuthManager } from "../infrastructure/auth/authManager";
-import { ConnectionManager } from "../infrastructure/connection/connectionManager";
-import { ApiClient } from "../infrastructure/http/apiClient";
-import { getConnectionDefaults } from "./connectionDefaults";
-
-export const connectionManager = new ConnectionManager(getConnectionDefaults());
-export const authManager = new AuthManager();
-export const apiClient = new ApiClient(connectionManager, authManager);
